77dac03826c4f7251f92986acfbcc30c4f923b8d
Sancho2003 <saneksuper178@yandex.ru>
2020-10-25 16:48:34 +0300

    'Fix'

--- Program.py
+++ Program.py
@@ -6,7 +6,6 @@ import os
 t = numpy.arange(0, 2*numpy.pi, 0.01)
 fig, ax = plt.subplots()
 ax.plot(numpy.sin(t)**3, numpy.cos(t)*13 - numpy.cos(2*t)*5 - numpy.cos(3*t)*2 - numpy.cos(4*t))
-plt.show()
 fig.savefig('1.png')
 f = Image.open('1.png')
 f.save('1.bmp', 'bmp')
